ba59f8b7e274ab299021eebd491f2e1a918ae984
OmarJaff <me@omarjaff.com>
2026-04-11 22:28:55 +0300

    latest

---
+++ resources/views/components/layout/layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Idea</title>
+    @vite(['/resources/css/app.css'])
+</head>
+<body class="bg-background text-forground">
+
+    <button class="btn">start</button>
+    <main>
+        {{$slot}}
+    </main>
+</body>
+</html>
