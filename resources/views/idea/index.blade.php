85b77b8995c4f26b3d5bea72761a8ca008fef5e5
OmarJaff <me@omarjaff.com>
2026-04-14 15:18:56 +0300

    latset

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div>
+    <div class="text-muted-foreground">
         <header class="py-8 md:py-12">
             <h1 class="text-3xl font-bold">Ideas</h1>
             <p class="text-muted-foreground text-sm mt-2">Capture your thoughts. Make a plan.</p>
@@ -8,9 +8,11 @@
             <div class="grid md:grid-cols-2 gap-6">
                 @forelse($ideas as $idea)
                     <x-card>
-                        <h3>
+                        <h3 class="text-foreground text-lg">
                             {{$idea->title}}
                         </h3>
+                        <div class="mt-5 line-clamp-3">{{$idea->description}}</div>
+                        <div class="mt-4">{{$idea->created_at->diffForHumans()}}</div>
                     </x-card>
                 @empty
                     <x-card>
