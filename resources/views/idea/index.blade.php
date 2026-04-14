c0f2918a9e93ba4d46dcd78028d5085e3716f4ff
OmarJaff <me@omarjaff.com>
2026-04-14 16:17:27 +0300

    Add status-label component

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -1,16 +1,21 @@
 <x-layout>
-    <div class="text-muted-foreground">
+    <div>
         <header class="py-8 md:py-12">
             <h1 class="text-3xl font-bold">Ideas</h1>
             <p class="text-muted-foreground text-sm mt-2">Capture your thoughts. Make a plan.</p>
         </header>
-        <div class="mt-10">
+        <div class="mt-10 text-muted-foreground">
             <div class="grid md:grid-cols-2 gap-6">
                 @forelse($ideas as $idea)
-                    <x-card>
+                    <x-card href="{{route('idea.show',$idea->id)}}">
                         <h3 class="text-foreground text-lg">
                             {{$idea->title}}
                         </h3>
+                        <div>
+                        <x-idea.idea-status status="{{$idea->status}}">
+                            {{$idea->status->label()}}
+                        </x-idea.idea-status>
+                        </div>
                         <div class="mt-5 line-clamp-3">{{$idea->description}}</div>
                         <div class="mt-4">{{$idea->created_at->diffForHumans()}}</div>
                     </x-card>
